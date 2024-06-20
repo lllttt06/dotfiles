a5e7fd9713e93b4fa9a8b27a9b7bdd5b67e2cd29
Koki Yoshida <yoshida_koki@cyberagent.co.jp>
2024-06-20 17:28:18 +0900

    update: copilot 追加

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -373,4 +373,29 @@ return {
             "nvim-lua/plenary.nvim",
         },
     },
+
+    -- Copilot
+    {
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        event = "InsertEnter",
+        config = function()
+            require("copilot").setup({
+                suggestion = {
+                    enabled = true,
+                    auto_trigger = true,
+                    hide_during_completion = true,
+                    debounce = 75,
+                    keymap = {
+                        accept = "<tab>",
+                        accept_word = false,
+                        accept_line = false,
+                        next = "<M-]>",
+                        prev = "<M-[>",
+                        dismiss = "<C-]>",
+                    },
+                },
+            })
+        end,
+    },
 }
