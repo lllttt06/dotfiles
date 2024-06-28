dbd014ace07fb9f3a47536476f2a2f1af6cf4e77
Koki Yoshida <yoshida_koki@cyberagent.co.jp>
2024-06-28 09:34:18 +0900

    tmp

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -148,6 +148,34 @@ return {
         config = function() require("nvim-highlight-colors").setup {} end
     },
 
+    -- カーソル位置ハイライト
+    {
+        "RRethy/vim-illuminate",
+        event = 'VeryLazy',
+        config = function()
+            require('illuminate').configure({
+                providers = {
+                    'lsp',
+                    'treesitter',
+                },
+                delay = 100,
+                filetype_overrides = {},
+                filetypes_denylist = {
+                    'dirvish',
+                    'fugitive',
+                },
+                filetypes_allowlist = {},
+                modes_denylist = {},
+                modes_allowlist = {},
+                providers_regex_syntax_denylist = {},
+                providers_regex_syntax_allowlist = {},
+                under_cursor = true,
+                large_file_cutoff = nil,
+                large_file_overrides = nil,
+                min_count_to_highlight = 1,
+            })
+        end
+    },
     -- スクロール
     {
         "karb94/neoscroll.nvim",
