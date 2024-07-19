1258aefabb79c8451be2eee20bee56efcb749a07
Koki Yoshida <lllttt06@gmail.com>
2024-07-19 16:09:26 +0900

    tmp

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -812,6 +812,27 @@ return {
                 fvm = false,
                 widget_guides = { enabled = true },
                 lsp = {
+                    on_attach = function(_, bufnr)
+                        local opts = { noremap = true, silent = true, buffer = bufnr }
+                        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+                        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+                        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+                        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+                        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+                        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+                        vim.keymap.set('n', '<space>wl', function()
+                            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                        end, opts)
+                        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+                        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+                        vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+                        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+                        vim.keymap.set('n', '<space>f', function()
+                            vim.lsp.buf.format { async = true }
+                        end, opts)
+                    end,
+                    capabilities = require("cmp_nvim_lsp").default_capabilities(),
                     settings = {
                         showtodos = true,
                         completefunctioncalls = true,
