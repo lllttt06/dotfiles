11df08a343fa517a43ca59e197c69a5635894587
Koki Yoshida <lllttt06@gmail.com>
2024-08-02 18:51:50 +0900

    tmp

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1295,7 +1295,7 @@ return {
 				vim.api.nvim_buf_clear_namespace(0, ns, 0, -1)
 				if vim.fn.mode() == 'i' and data.status == 'InProgress' then
 					vim.api.nvim_buf_set_extmark(0, ns, vim.fn.line '.' - 1, 0, {
-						virt_text = { { '  Thinking...', 'Comment' } },
+						virt_text = { { '  ...', 'Comment' } },
 						virt_text_pos = 'eol',
 						hl_mode = 'combine',
 					})
