11dfbf595a3907c70c4d9fe4da71f5c2375e11bf
Koki Yoshida <yoshida_koki@cyberagent.co.jp>
2024-10-05 15:31:29 +0900

    feat: CopilotChat.nvimプラグインを追加

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1167,6 +1167,115 @@ return {
             }
         end,
     },
+    {
+        "CopilotC-Nvim/CopilotChat.nvim",
+        branch = "canary",
+        event = "VeryLazy",
+        dependencies = {
+            { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+            { "nvim-lua/plenary.nvim" },  -- for curl, log wrapper
+        },
+        build = "make tiktoken",          -- Only on MacOS or Linux
+        keys = {
+            { "<leader>cc", "<cmd>CopilotChatToggle<cr>", desc = "CopilotChat" },
+            { "<leader>co", "<cmd>CopilotChatCommit<cr>", desc = "CopilotChat" },
+            { "<leader>cr", "<cmd>CopilotChatReview<cr>", desc = "CopilotChat" }
+        },
+        config = function()
+            local select = require('CopilotChat.select')
+            require('CopilotChat').setup {
+                debug = true,               -- Enable debugging
+                window = {
+                    layout = 'float',       -- 'vertical', 'horizontal', 'float', 'replace'
+                    width = 0.7,            -- fractional width of parent, or absolute width in columns when > 1
+                    height = 0.7,           -- fractional height of parent, or absolute height in rows when > 1
+                    relative = 'editor',    -- 'editor', 'win', 'cursor', 'mouse'
+                    border = 'rounded',     -- 'none', single', 'double', 'rounded', 'solid', 'shadow'
+                    row = nil,              -- row position of the window, default is centered
+                    col = nil,              -- column position of the window, default is centered
+                    title = 'Copilot Chat', -- title of chat window
+                    footer = nil,           -- footer of chat window
+                    zindex = 1,             -- determines if window is on top or below other floating windows
+                },
+                prompts = {
+                    Explain = {
+                        prompt = '/COPILOT_EXPLAIN Write an explanation for the active selection as paragraphs of text.',
+                    },
+                    Fix = {
+                        prompt =
+                        '/COPILOT_GENERATE There is a problem in this code. Rewrite the code to show it with the bug fixed.',
+                    },
+                    Optimize = {
+                        prompt = '/COPILOT_GENERATE Optimize the selected code to improve performance and readability.',
+                    },
+                    Docs = {
+                        prompt = '/COPILOT_GENERATE Please add documentation comment for the selection.',
+                    },
+                    Tests = {
+                        prompt = '/COPILOT_GENERATE Please generate tests for my code.',
+                    },
+                    FixDiagnostic = {
+                        prompt = 'Please assist with the following diagnostic issue in file:',
+                        selection = select.diagnostics,
+                    },
+                    Commit = {
+                        prompt =
+                        'Write commit message for the change with commitizen convention in Japanese. Make sure the title has maximum 50 characters and message is wrapped at 72 characters. Wrap the whole message in code block with language gitcommit.',
+                        selection = select.gitdiff,
+                    },
+                    CommitStaged = {
+                        prompt =
+                        'Write commit message for the change with commitizen convention in Japanese. Make sure the title has maximum 50 characters and message is wrapped at 72 characters. Wrap the whole message in code block with language gitcommit.',
+                        selection = function(source)
+                            return select.gitdiff(source, true)
+                        end,
+                    },
+                    Review = {
+                        prompt = '/COPILOT_REVIEW Review the selected code and answer in Japanese.',
+                        callback = function(response, source)
+                            local ns = vim.api.nvim_create_namespace('copilot_review')
+                            local diagnostics = {}
+                            for line in response:gmatch('[^\r\n]+') do
+                                if line:find('^line=') then
+                                    local start_line = nil
+                                    local end_line = nil
+                                    local message = nil
+                                    local single_match, message_match = line:match('^line=(%d+): (.*)$')
+                                    if not single_match then
+                                        local start_match, end_match, m_message_match = line:match(
+                                            '^line=(%d+)-(%d+): (.*)$')
+                                        if start_match and end_match then
+                                            start_line = tonumber(start_match)
+                                            end_line = tonumber(end_match)
+                                            message = m_message_match
+                                        end
+                                    else
+                                        start_line = tonumber(single_match)
+                                        end_line = start_line
+                                        message = message_match
+                                    end
+
+                                    if start_line and end_line then
+                                        table.insert(diagnostics, {
+                                            lnum = start_line - 1,
+                                            end_lnum = end_line - 1,
+                                            col = 0,
+                                            message = message,
+                                            severity = vim.diagnostic.severity.WARN,
+                                            source = 'Copilot Review',
+                                        })
+                                    end
+                                end
+                            end
+                            vim.diagnostic.set(ns, source.bufnr, diagnostics)
+                        end,
+                    },
+                },
+
+
+            }
+        end
+    },
 
     -- snippet
     {
