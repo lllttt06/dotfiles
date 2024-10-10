f0b3eb2700abdd572d9afee1cb67b78d4b5a648f
Koki Yoshida <lllttt06@gmail.com>
2024-10-10 14:11:30 +0900

    tmp

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1440,18 +1440,18 @@ return {
         config = true,
     },
 
-    -- {
-    --     "rachartier/tiny-code-action.nvim",
-    --     dependencies = {
-    --         { "nvim-lua/plenary.nvim" },
-    --         { "nvim-telescope/telescope.nvim" },
-    --     },
-    --     event = "LspAttach",
-    --     config = function()
-    --         require('tiny-code-action').setup()
-    --         vim.keymap.set("n", "<leader>ca", function()
-    --             require("tiny-code-action").code_action()
-    --         end, { noremap = true, silent = true })
-    --     end
-    -- },
+    {
+        "rachartier/tiny-code-action.nvim",
+        dependencies = {
+            { "nvim-lua/plenary.nvim" },
+            { "nvim-telescope/telescope.nvim" },
+        },
+        event = "LspAttach",
+        config = function()
+            require('tiny-code-action').setup()
+            vim.keymap.set("n", "<leader>ca", function()
+                require("tiny-code-action").code_action()
+            end, { noremap = true, silent = true })
+        end
+    },
 }
