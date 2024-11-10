9cefa8c2555543f7282e84eb744ee81c81729cdb
Koki Yoshida <yoshida_koki@cyberagent.co.jp>
2024-11-10 16:51:03 +0900

    feat: tiny-code-action 復活

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -710,7 +710,7 @@ return {
                     end, opts)
                     vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
                     vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-                    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+                    -- vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
                     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
                     vim.keymap.set('n', '<space>f', function()
                         vim.lsp.buf.format { async = true }
@@ -1406,6 +1406,20 @@ return {
         event = 'VeryLazy',
         version = "*",
         config = true,
+    },
+    {
+        "rachartier/tiny-code-action.nvim",
+        dependencies = {
+            { "nvim-lua/plenary.nvim" },
+            { "nvim-telescope/telescope.nvim" },
+        },
+        event = "VeryLazy",
+        config = function()
+            require('tiny-code-action').setup()
+            vim.keymap.set("n", "<leader>ca", function()
+                require("tiny-code-action").code_action()
+            end, { noremap = true, silent = true })
+        end
     },
     -- dynamic macro
     {
