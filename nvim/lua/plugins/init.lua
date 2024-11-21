fc1ba97ec7e19497b3306902596e29e02fca9447
Koki Yoshida <yoshida_koki@cyberagent.co.jp>
2024-11-21 09:58:22 +0900

    tmp

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -849,11 +849,11 @@ return {
             vim.opt.completeopt = { "menu", "menuone", "noselect" }
             vim.o.completefunc = 'v:lua.require("cmp").complete()'
 
-            -- local has_words_before = function()
-            --     if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
-            --     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-            --     return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
-            -- end
+            local has_words_before = function()
+                if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+                local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+                return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+            end
 
             cmp.setup({
                 snippet = {
